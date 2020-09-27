924cd8c912bd581dea4eff86c0fed57728c8b5a5
Almen Ng <68764826+almen-ng@users.noreply.github.com>
2020-09-27 03:15:15 -0400

    Initial commit

---
+++ app/src/main/java/com/example/travelsafe/DataStructures.java
@@ -0,0 +1,23 @@
+package com.example.travelsafe;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * @author Veerash Palanichamy
+ * DataStructures Class
+ * Static Variable Declarations
+ */
+public class DataStructures {
+    public static ArrayList<State> states;
+    public static ArrayList<String> stateNames;
+    public static Graph graph;
+    public static HashMap<String, ArrayList<String>> weatherGrouping;
+
+    public static boolean statesInitialized;
+    public static boolean stateNamesInitialized;
+    public static boolean graphInitialized;
+    public static boolean weatherGroupingInitialized;
+
+
+}
